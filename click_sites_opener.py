37ee8996447ce0859d88370a3bad4d78e276db25
Jaber Haisan <jaberhaisan@gmail.com>
2021-12-22 20:30:01 -0700

    Spacing changes
    Made it easier to comment out unwanted websites in sites by making websites more spaced out.

--- click_sites_opener.py
+++ click_sites_opener.py
@@ -1,8 +1,15 @@
 import webbrowser
 
-sites = ["https://www.allureofnds.net/", "http://dragonbreederscave.com/index.php?page=hatcheryII",
-	"http://hatching.club/", "http://greg-kennedy.com/dragcave/",
-	"http://dc.evinext.com/", "http://lair.silverdrak.de/", "http://valleysherwood.com/"]
-	
+sites = [
+	"https://www.allureofnds.net/daycare", 
+	"http://dragonbreederscave.com/index.php?page=hatcheryII",
+	"http://hatching.club/", 
+	"http://lair.silverdrak.de/"
+	"http://dc.evinext.com/"
+	"http://greg-kennedy.com/dragcave/", 
+	"http://valleysherwood.com/"
+	]
+
+
 for site in sites:
 	webbrowser.open(site)
