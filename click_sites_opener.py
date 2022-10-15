f3f25a8f1dd64d89e57eb773551ac40813d1dbd3
Jaber Haisan <jaberhaisan@gmail.com>
2022-10-15 13:22:06 -0600

    Removed dead link and added missing commas
    The website to dragonbreederscave seems to have been abandoned so was removed from the list of sites

--- click_sites_opener.py
+++ click_sites_opener.py
@@ -2,10 +2,9 @@ import webbrowser
 
 sites = [
 	"https://www.allureofnds.net/daycare", 
-	"http://dragonbreederscave.com/index.php?page=hatcheryII",
 	"http://hatching.club/", 
-	"http://lair.silverdrak.de/"
-	"http://dc.evinext.com/"
+	"http://lair.silverdrak.de/",
+	"http://dc.evinext.com/",
 	"http://greg-kennedy.com/dragcave/", 
 	"http://valleysherwood.com/"
 	]
